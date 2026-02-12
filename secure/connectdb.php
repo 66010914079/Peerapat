b2e96ad659e6be55d9087ffeb861ffca9c8755be
66010914079 <66010914079@msu.ac.th>
2026-02-13 00:28:29 +0700

    remove duplicate secure folder and move h_secure to root

--- secure/connectdb.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "Pw_660109140636769", "4079db");
-if (!$conn) { 
-    die("Connection failed: " . mysqli_connect_error()); 
-}
-mysqli_query($conn, "SET NAMES utf8"); // ตั้งค่ารองรับภาษาไทย
-?>
