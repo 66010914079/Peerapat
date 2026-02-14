dfba0946341ec8349427d635dd914a23e61e5c89
66010914079 <66010914079@msu.ac.th>
2026-02-14 17:06:28 +0700

    Update connectdb.php

--- e/connectdb.php
+++ e/connectdb.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "Pw_660109140636769", "4079db");
+$conn = mysqli_connect("localhost", "root", "", "4079db");
 if (!$conn) { 
     die("Connection failed: " . mysqli_connect_error()); 
 }
