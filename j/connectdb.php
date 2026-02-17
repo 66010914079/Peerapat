9b9a7ec9eeb61153f586dc685df59b86361d802a
66010914079 <66010914079@msu.ac.th>
2026-02-17 14:19:01 +0700

    Update connectdb.php

--- j/connectdb.php
+++ j/connectdb.php
@@ -1,7 +1,14 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "Pw_660109140636769", "4079db");
-if (!$conn) { 
-    die("Connection failed: " . mysqli_connect_error()); 
+$servername = "localhost";
+$username = "root"; // ถ้ายังไม่ได้ ให้ลองถามอาจารย์ว่ามี User เฉพาะโปรเจกต์ไหม
+$password = "Pw_660109140636769"; 
+$dbname = "4079db";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    // แก้ให้แสดง Error ที่ละเอียดขึ้นเพื่อหาจุดผิด
+    die("Connection failed: " . mysqli_connect_error());
 }
-mysqli_query($conn, "SET NAMES utf8"); // ตั้งค่ารองรับภาษาไทย
+mysqli_query($conn, "SET NAMES utf8");
 ?>
